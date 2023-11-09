56872233184e086429680a2e1c15973e9c297e9e
Taam Paramee <taamtera@gmail.com>
2023-11-09 10:10:30 +0700

    manual gen comb list

--- combination_gen.py
+++ combination_gen.py
@@ -1,9 +1,13 @@
-from itertools import product
+# from itertools import product
+# gen_comb_list = (lambda list_set: list(product(*list_set)))
 
+def gen_comb_list(list_set, ls: list = [], a: list = []):
+    for i in list_set[0]:
+        if len(list_set) == 1:
+            a.append(ls + [i])
+        else:
+            gen_comb_list(list_set[1:], ls + [i])
+    return a
 
-def gen_comb_list(list_set):
-    return list(product(*list_set))
-
-
-print(gen_comb_list([[1, 2, 3]]))
-print(gen_comb_list([[1, 2, 3], [4, 5]]))
+# print(gen_comb_list([[1, 2, 3]]))
+print(gen_comb_list([[1, 2, 3], [4, 5], [4, 5]]))
