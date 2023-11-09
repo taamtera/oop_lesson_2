15bd9df9da2e4c10285e6e4b7990bb463c90287a
Taam Paramee <taamtera@gmail.com>
2023-11-09 11:44:29 +0700

    fix combi list again

--- combination_gen.py
+++ combination_gen.py
@@ -2,12 +2,12 @@
 # gen_comb_list = (lambda list_set: list(product(*list_set)))
 
 def gen_comb_list(list_set, ls: list = [], a: list = []):
-    for i in list_set[0]:
-        if len(list_set) == 1:
-            a.append(ls + [i])
+    for i in list_set[-1]:
+        if len(list_set) == 1:          
+            a.append([i] + ls )
         else:
-            gen_comb_list(list_set[1:], ls + [i])
+            gen_comb_list(list_set[0:-1],  [i] + ls)
     return a
 
-# print(gen_comb_list([[1, 2, 3]]))
-print(gen_comb_list([[1, 2, 3], [4, 5], [4, 5]]))
+print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
+# print(gen_comb_list([[1, 2, 3], [4, 5], [4, 5]]))
